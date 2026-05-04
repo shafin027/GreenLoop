aebc007403f987967c68edfe379a6d15a3a09eca
SHAFIN MAHAMUD <126391493+shafin027@users.noreply.github.com>
2026-05-04 22:35:33 +0600

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/AuthController';
+import { login, register, adminSignupAvailable } from '../controllers/AuthController';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/admin-available', adminSignupAvailable);
 
 export default router;
